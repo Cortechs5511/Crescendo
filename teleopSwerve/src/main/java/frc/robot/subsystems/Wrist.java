a667482b4f01a6f1c44cb6ea3b3992ae20b68e21
Ryan-919 <68666490+Ryan-919@users.noreply.github.com>
2024-03-02 18:43:56 -0500

    Create Wrist.java

---
+++ teleopSwerve/src/main/java/frc/robot/subsystems/Wrist.java
@@ -0,0 +1,72 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.WristConstants;
+
+public class Wrist extends SubsystemBase {
+    private final CANSparkMax wristLeft = createWristController(WristConstants.WRIST_L_ID, true);
+    private final CANSparkMax wristRight = createWristController(WristConstants.WRIST_R_ID, false);
+    
+    private final PIDController wristPIDController = new PIDController(0.03, 0, 0);
+
+
+    private final DutyCycleEncoder absoluteEncoder = new DutyCycleEncoder(WristConstants.THROUGH_BORE_ID);
+
+    public Wrist() {
+
+    }
+    
+    public void zeroEncoder() {
+        absoluteEncoder.reset();
+    }
+    
+    public double getRawPosition() {
+        return Math.abs(absoluteEncoder.getAbsolutePosition());
+    }
+
+    public double getPosition() {
+        return WristConstants.MIN_POS+(absoluteEncoder.getAbsolutePosition()/(WristConstants.MIN_POS-WristConstants.MAX_POS));
+    }
+
+    public void setPower(double power) { //wyan
+        wristLeft.set(power);
+        wristRight.set(power);
+    }
+
+    public void setPosition(double position) {
+        final double wristOutput = wristPIDController.calculate(absoluteEncoder.getAbsolutePosition(), position);
+        wristLeft.set(wristOutput);
+        wristRight.set(wristOutput);
+    }
+    
+    private CANSparkMax createWristController(int port, boolean isInverted) {
+        CANSparkMax controller = new CANSparkMax(port, MotorType.kBrushless);
+        controller.restoreFactoryDefaults();
+
+        controller.enableVoltageCompensation(WristConstants.VOLTAGE_COMPENSATION);
+        controller.setIdleMode(WristConstants.IDLE_MODE);
+        controller.setOpenLoopRampRate(WristConstants.RAMP_RATE);
+        controller.setClosedLoopRampRate(WristConstants.RAMP_RATE); 
+
+        controller.setSmartCurrentLimit(WristConstants.CURRENT_LIMIT);
+
+        controller.setInverted(isInverted);
+        return controller;
+    }
+    
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Wrist/Absolute Encoder Value", getRawPosition());
+        SmartDashboard.putNumber("Wrist/Persentage PositionValue", getPosition());
+    }
+}
